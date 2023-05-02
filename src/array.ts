ece975077ce2bc31e859db90bca43a27830fcf1e
syed abdul hakim <s.abdulhakim10@gmail.com>
2023-05-02 22:04:38 +0600

    Array and Tuples in typescript

---
+++ src/array.ts
@@ -0,0 +1,8 @@
+const names : string[] = ["Ameer", "Omayer", "Qamrul"];
+const rollNumbers : number[] = [28, 24, 26];
+
+const nameList = ["abul", "kabul", 24, true];
+
+// Tuples
+const user : [number, string] = [333, "Mezba"];
+const couple: [string, boolean] = ["Tusar", false];
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
 const course: string = "Next Level Web Development";
 console.log(course);
+
+/**
+ * Primitive Types
+ * 1. string
+ * 2. number
+ * 3. boolean
+ * 4. null
+ * 5. undefined
+ */
