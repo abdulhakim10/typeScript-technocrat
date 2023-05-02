814b336293fd6cd1856a808d1f1714f96cf71a73
syed abdul hakim <s.abdulhakim10@gmail.com>
2023-05-02 23:09:21 +0600

    object, Literal types and Optional types

---
+++ src/object.ts
@@ -0,0 +1,14 @@
+const newUser : {
+    // literal type: specific value declaration
+    company: 'Programming Hero';
+    name: string;
+    age: number;
+    isMarried: boolean;
+    wife?: string;
+} = {
+    company: "Programming Hero",
+    name: "Fahim",
+    age: 22,
+    isMarried: true,
+    wife: "Afsana"
+}
