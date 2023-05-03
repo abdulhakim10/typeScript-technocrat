107f63768b1c17be2b92393c91519140120d5e6b
syed abdul hakim <s.abdulhakim10@gmail.com>
2023-05-03 10:13:34 +0600

    Null, unknown, never types

---
+++ src/nullable-unknown-never.ts
@@ -0,0 +1,37 @@
+// nullable types
+const searchName = (value: string | null) => {
+    if (value === null) {
+        console.log("There is nothing to search.");
+    }
+    else {
+        console.log("Searching...");
+    }
+}
+
+searchName(null)
+
+// unknown type
+
+const getCarSpeed = (speed: unknown) => {
+    if (typeof speed === "number") {
+        const convertedSpeed = (speed * 1000) / 3600;
+        console.log(`Car speed is ${convertedSpeed}`);
+    }
+    if (typeof speed === "string") {
+        const [value, unit] = speed.split(" "); //["10", "kmh^-1"]
+        const convertedSpeed = (parseFloat(value) * 1000) / 3600;
+        console.log(`Car speed is ${convertedSpeed}`);
+    }
+}
+
+getCarSpeed(10);
+getCarSpeed("10 kmh^-1");
+
+
+// never type
+// there is nothing to return
+function throwError(message: string): never {
+    throw new Error(message)
+}
+
+throwError("Error Hoyeche");
