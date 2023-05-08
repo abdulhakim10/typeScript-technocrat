ae4dba40d496447858316bbc79882d4480407ed9
syed abdul hakim <s.abdulhakim10@gmail.com>
2023-05-08 11:17:58 +0600

    Inheritence

--- src/module4/object.ts
+++ src/module4/object.ts
@@ -1,34 +1,34 @@
-// class Animal{
-// //   public name: string;
-//     // public species: string;
-//     // public sound: string;
+// // class Animal{
+// // //   public name: string;
+// //     // public species: string;
+// //     // public sound: string;
 
-//     // Parameter Properties
-//     constructor(public name: string, public species: string, public sound: string){
-//         // this.name = name;
-//         // this.species = species;
-//         // this.sound = sound;
-//     }
-//    public makeSound(){
-//         console.log(`The ${this.name} says ${this.sound}`);
+// //     // Parameter Properties
+// //     constructor(public name: string, public species: string, public sound: string){
+// //         // this.name = name;
+// //         // this.species = species;
+// //         // this.sound = sound;
+// //     }
+// //    public makeSound(){
+// //         console.log(`The ${this.name} says ${this.sound}`);
+// //     }
+// // }
+
+// class Animal {
+
+//     // parameter properties
+//     constructor(
+//         public name: string,
+//         public species: string,
+//         public sound: string
+//     ){}
+
+//     public makeSound() {
+//         console.log(`The ${this.name} ${this.species} says ${this.sound}`);
 //     }
 // }
 
-class Animal {
-    
-    // parameter properties
-    constructor(
-        public name: string,
-        public species: string,
-        public sound: string
-    ){}
-
-    public makeSound() {
-        console.log(`The ${this.name} ${this.species} says ${this.sound}`);
-    }
-}
-
-const dog = new Animal("German Shepard", "dog", "Ghew Ghew");
-const cat = new Animal("Boro lom wala", "cat", "Mew Mew");
-dog.makeSound();
-cat.makeSound();
+// const dog = new Animal("German Shepard", "dog", "Ghew Ghew");
+// const cat = new Animal("Boro lom wala", "cat", "Mew Mew");
+// dog.makeSound();
+// cat.makeSound();
